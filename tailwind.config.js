3f0e012f38f7ba8de71b06afda83d54eba215dae
Pedro <p3droegarcia@gmail.com>
2023-06-21 15:13:00 -0600

    Netflix Theme Configuration
    Set main theme colors with daisyUI.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  daisyui: {
+    themes: [
+      {
+        netflix: {
+          primary: "#e50914",
+          "primary-content": "#000000",
+          secondary: "#221f1f",
+          accent: "#c149ad",
+          neutral: "#ffffff",
+          "base-100": "#f5f5f1",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
